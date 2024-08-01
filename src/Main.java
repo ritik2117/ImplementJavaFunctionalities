36c09bec561cac708b3c49247cdf062f006e66df
ritik2117 <ritikgupta21.17@gmail.com>
2024-08-01 23:09:18 +0530

    Added values in list

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(1);
+        integerList.add(2);
+        integerList.add(3);
+        integerList.add(4);
+        System.out.println(integerList.indexOf(4));
+    }
+}
